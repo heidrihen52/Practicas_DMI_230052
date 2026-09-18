1e1cc76ed8284b3a60b835081beb56e3e775da75
Adrian Perez Jimenez <230052@utxicotepec.edu.mx>
2026-09-18 16:12:24 -0600

    Practica03: YesNo App
    Add: Creacion y configuracion de la tercera practica de la materia

---
+++ Practica03/yes_no_app/lib/presentation/screens/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7aqhB8IeiFzeS9rRTuS1MfmhD-dKxgr-NnpJzZLVnMA&s=10'),
+          ),
+        ),
+        title: const Text('hp'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return  MyMessageBubble();
+                    })),
+
+            /// Caja de texto de mensajes
+            //const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
